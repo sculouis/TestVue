dc3c0fbfe988b75fcfdd7bf35a71da4f01998626
Louis Chen <scu.louis@gmail.com>
2023-02-01 14:52:21 +0800

    add vuetify

---
+++ src/views/TodoView.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import TodoList from "@/components/TodoList.vue";
+import { useTodoStore } from "@/stores/todos";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+const name = ref("");
+const store = useTodoStore();
+const {todos} = storeToRefs(store);
+const {addTodo} = store;
+</script>
+
+<template>
+    <form @submit.prevent="addTodo(name)">
+      <div class="flex items-center justify-center">
+        <input
+          id="todo-input"
+          class="border-b outline-none w-96 px-4 py-2"
+          type="text"
+          v-model="name"
+          placeholder="Please input todo name..."
+        />
+        <button class="bg-blue-500 text-white px-4 py-2">Add</button>
+      </div>
+    </form>
+    <TodoList></TodoList>
+</template>
